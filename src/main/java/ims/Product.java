8e106b85a0e3291c6df16bc53227fd7a343cf090
Warsame Ahmed <wahmed04@dcs.bbk.ac.uk>
2020-04-18 13:43:19 +0100

    Created a 'Main' class and a 'Product' interface

---
+++ src/main/java/ims/Product.java
@@ -0,0 +1,16 @@
+package ims;
+
+public interface Product {
+	
+	public void setID();
+	
+	public int getID();
+	
+	public void setName();
+	
+	public String getName();
+	
+	public void setBrand();
+	
+	public String getBrand();
+}
